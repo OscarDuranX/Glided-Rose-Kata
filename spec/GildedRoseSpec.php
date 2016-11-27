a6718edbd18d594f8ac35d7bad0d922702a520f5
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-11-27 14:08:50 +0100

    Sync

---
+++ spec/GildedRoseSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace spec\App;
+
+use App\GildedRose;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class GildedRoseSpec
+ * @package spec\App
+ */
+class GildedRoseSpec extends ObjectBehavior
+{
+    /**
+     * Check it is initializable
+     */
+    function it_is_initializable()
+    {
+        $this->beConstructedWith('normal',10,5);
+        $this->shouldHaveType(GildedRose::class);
+    }
+
+    /**
+     * Check it is initializable
+     */
+    function it_updates_normal_items_before_sell_date()
+    {
+        $this->beConstructedWith('normal',10,5);
+
+        $this->tick();
+
+        $this->quality->shouldBe(9);
+        $this->sellIn->shouldBe(4);
+    }
+}
