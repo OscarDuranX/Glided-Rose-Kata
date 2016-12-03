9f8c6a2253eeb042d51831bf12e56ad8ccd0a6ec
Oscar Duran <xoscarduranx@gmail.com>
2016-12-03 19:25:22 +0100

    sync

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -55,4 +55,96 @@ class GildedRoseSpec extends ObjectBehavior
         $this->quality->shouldBe(8);
         $this->sellIn->shouldBe(-6);
     }
+
+    function it_updates_normal_items_with_a_quality_of_0()
+    {
+
+        $this->beConstructedThrough('of',['normal',0,5]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(0);
+        $this->sellIn->shouldBe(4);
+    }
+
+    function it_updates_Brie_items_before_the_sell_date()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',10,5]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(11);
+        $this->sellIn->shouldBe(4);
+    }
+
+    function it_updates_Brie_items_before_the_sell_date_with_maximum_quality()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',50,5]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(4);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',10,0]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(12);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date_near_maximum_quality()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',49,0]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date_with_maximum_quality()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',50,0]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_after_the_sell_date()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',10,-10]);
+
+        $this->tick();
+
+//        $this->quality->shouldBe(12);
+//        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_after_the_sell_date_with_maximum_quality()
+    {
+
+        $this->beConstructedThrough('of',['Aged Brie',50,-10]);
+
+        $this->tick();
+
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-11);
+    }
+
+
+
+
 }
